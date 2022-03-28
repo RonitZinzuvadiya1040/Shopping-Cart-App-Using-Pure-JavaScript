712ddb48ca8d0c6aa677b80d893f48e8d370dd53
Ronit Zinzuvadiya <96730598+RonitZinzuvadiya1040@users.noreply.github.com>
2022-03-28 12:46:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,84 @@
-
-
-fetch('https://fakestoreapi.com/products').then((data) => {
+fetch("https://fakestoreapi.com/products")
+  .then((data) => {
     // console.log(data);
     return data.json();
-}).then((completedata)=> {
+  })
+  .then((completedata) => {
     // console.log(completedata[2].title);
     // document.getElementById('root').innerHTML=completedata[2].title;
-    let data1='';
-    completedata.map((values)=>{
-        data1+=`<div class="card">
-        <h1 class="title">${values.title}</h1><br/>
+    let data1 = "";
+    completedata.map((values) => {
+      data1 += `<div class="card">
+        
+      <h1 class="title">${values.title}</h1><br/>
         <img src=${values.image} alt="img" class="images"><br/><br/>
         <b>Description:</b>
         <p>${values.description}</p><br/>
-        <p class="category">Category:   ${values.category}</p>
-        <p class="price">Price: ₹${values.price}</p>
-        <button class="btn">Add to Cart</button> 
-        <button class="btn">Edit</button> 
+        <p class="category">Category: ${values.category}</p>
+        <p class="price">Price: $${values.price}</p>
+        
+        <form method="get" action="/addtocart.html">
+        <button class="btn" type="submit" id="cart">Add to Cart</button> 
+        </form>
+        
+        <form method="get" action="/editproduct.html">
+        <button class="btn" type="submit" id="editproduct">Edit Product</button>
+        </form>
+
     </div>`;
-    
     });
 
-    document.getElementById("cards").innerHTML=data1;
-
-
-}).catch((err) => {
+    document.getElementById("cards").innerHTML = data1;
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
+// For Add Product
+fetch("https://fakestoreapi.com/products", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "test product",
+    price: 13.5,
+    description: "lorem ipsum set",
+    image: "https://i.pravatar.cc",
+    category: "electronic",
+  }),
+})
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+
+// For Update Product
+fetch("https://fakestoreapi.com/products/6", {
+  method: "PUT",
+  body: JSON.stringify({
+    title: "test product",
+    price: 13.5,
+    description: "lorem ipsum set",
+    image: "https://i.pravatar.cc",
+    category: "electronic",
+  }),
+})
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+
+// For Delete Product
+fetch("https://fakestoreapi.com/products/6", {
+  method: "DELETE",
+})
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+
+// Get all categories
+fetch("https://fakestoreapi.com/products/categories")
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+
+
+
+
+
+  
 // For checking only
 // fetch('https://fakestoreapi.com/products/categories').then((data) => {
 //     return data.category.json();
@@ -46,7 +97,6 @@ fetch('https://fakestoreapi.com/products').then((data) => {
 //             </div>`;
 //     });
 //     document.getElementById("cards").innerHTML=data2;
-
 
 // }).catch((err) => {
 //     console.log(err);
